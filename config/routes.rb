105f08b3248077bd97890c4187aa7ee3ba158f3b
blurden <blurden@gmail.com>
2013-02-28 14:20:43 -0800

    Add Pages Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Innercircle::Application.routes.draw do
+  
+
+
+  root :to => 'pages#home'
+  #get "pages/home"
+  get 'about' => 'pages#about'
+
+
+
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
