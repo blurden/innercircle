b820c30bf01037f4fc42c0a0b5b9d790063903ac
blurden <blurden@gmail.com>
2013-02-28 10:44:37 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Innercircle::Application.config.secret_token = 'e61d4fcbe1de09145c4517702531d39bd9e29ba9d39fbf1811362c650509b3299e9f1abe39604bb10556db94ec79c388780ec2bd5486c1e0dff0209660a2e20f'
