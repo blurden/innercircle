d79dd5b914c43cb28bb60c8f225c9cc8c2c92f90
blurden <blurden@gmail.com>
2013-03-03 12:00:46 -0800

    fix ignore

--- config/initializers/dev_environment.rb
+++
@@ -1,6 +0,0 @@
-unless Rails.env.production?
-  ENV['TWITTER_CONSUMER_KEY'] = 'ZP1pQRxqZ7Z30w7bvoKRHA'
-  ENV['TWITTER_CONSUMER_SECRET'] = 'nkj7icluAuwBX42xGIUE9FoSOvGFMUHxaq9VkuU'
-  ENV['GOOGLE_OA2_KEY'] = '12757997247.apps.googleusercontent.com'
-  ENV['GOOGLE_OA2_SECRET'] = 'DUmAaJFpy7Izsf71W0rDX32s'
-end
