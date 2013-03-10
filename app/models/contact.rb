533995f83010c82ea373930fb4fcab3584f15e9e
blurden <blurden@gmail.com>
2013-03-09 18:11:21 -0800

    add carrierwave file cache

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,16 +1,18 @@
 class Contact < ActiveRecord::Base
-  attr_accessible :name, :email, :phone, :image, :file, :remote_file_url
+  attr_accessible :name, :email, :phone, :image, :file, :remote_file_url, :file_cache
   mount_uploader :file, FileUploader
 
   validates :name, presence: true
   validates :email, :presence => true, :email => true
-  validates :phone, :presence => true, :phone => true, :phony_plausible => true
+  validates :phone, :presence => true, :phone => true
+  validates :file, :presence => true
   validates :user_id, presence: true
+
 
   belongs_to :user
 
-  # Normalizes attribute before validation and saves into other attribute
-  phony_normalize :phone, :default_country_code => 'US'
+  ## Normalizes attribute before validation and saves into other attribute
+  #phony_normalize :phone, :default_country_code => 'US'
 
 
 
